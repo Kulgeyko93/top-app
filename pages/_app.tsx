287388edb4071265df63b8573c5dce1daa95d9b1
aleksey93 <kulgeyko93@mail.ru>
2021-12-03 16:47:33 +0300

    feat: head element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import Head from 'next/head';
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+        <Head>
+        <title>My top</title>
+        <link key={1} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
