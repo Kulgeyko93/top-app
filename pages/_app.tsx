3cc34bc3404126a032883d7cdc5efbe3e2aa3492
alex_ey123@mail.ru <aliaksei.kulheika@yellow.systems>
2021-12-04 18:41:10 +0300

    feat: add Htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,10 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <>
         <Head>
-        <title>My top</title>
-        <link key={1} rel="icon" href="/favicon.ico" />
+          <title>My top</title>
+          <link key={1} rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
   </>;
